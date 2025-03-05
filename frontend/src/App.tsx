dbf92919b893756165e43aa59a50ea76a8bbbf27
JhoannS <sebastianzamudio2004@gmail.com>
2025-03-05 16:58:24 -0500

    Adelanto de css manual, pendiente de revisar los estilos de tailwind, algunos no cogen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,8 @@ function App() {
     <>
     <div className="h-screen flex flex-col justify-center items-center bg-bg-empty text-mono-blanco">
       <h1 className="text-4xl font-bold text-semaforo-verde">¡Hola, mundo! 🚀</h1>
-      <p className="text-semaforo-rojo shadow p-2">Este es un texto en rojo.</p>
-      <button className="px-4 py-2 mt-4 bg-shopper-primary text-white rounded-md">
+      <p className="text-essentials-primary shadow p-2 mb-100">Este es un texto en rojo.</p>
+      <button className="mt-10 bg-shopper-primary text-white rounded-md">
         Botón Shopper
       </button>
     </div>
