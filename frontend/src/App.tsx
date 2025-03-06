52540f346da15225f519b9ea636a963b16925a83
JhoannS <sebastianzamudio2004@gmail.com>
2025-03-06 07:30:24 -0500

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-    <div className="h-screen flex flex-col justify-center items-center bg-bg-empty text-mono-blanco">
-      <h1 className="text-4xl font-bold text-semaforo-verde">¡Hola, mundo! 🚀</h1>
+    <div className="h-screen flex flex-col justify-center items-center text-semaforo-verde">
+      <h1 className="text-4xl font-bold ">¡Hola, mundo! 🚀</h1>
       <p className="text-essentials-primary shadow p-2 mb-100">Este es un texto en rojo.</p>
       <button className="mt-10 bg-shopper-primary text-white rounded-md">
         Botón Shopper
