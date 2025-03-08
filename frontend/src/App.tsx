b3e135943a8b0ac8dc2587aed6d8bd89e3ad47c8
JhoannS <sebastianzamudio2004@gmail.com>
2025-03-07 23:10:45 -0500

    nada,solo cumplimiento

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
     <div className="h-screen flex flex-col justify-center items-center bg-bg-empty text-mono-blanco">
       <h1 className="text-4xl font-bold text-semaforo-verde">¡Hola, mundo! 🚀</h1>
-      <p className="text-essentials-primary shadow p-2 mb-100">Este es un texto en rojo.</p>
+      <p className="text-essentials-primary shadow px-3">Este es un texto en rojo pero no funciona el padding.</p>
       <button className="mt-10 bg-shopper-primary text-white rounded-md">
         Botón Shopper
       </button>
