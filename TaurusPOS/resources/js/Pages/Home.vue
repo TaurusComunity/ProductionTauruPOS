a0a0b458d6942cb811bf47f34453e2440b4e72ca
JhoannS <sebastianzamudio2004@gmail.com>
2025-03-09 22:25:00 -0500

    Funcionando servidor junto a renderizado de Vue

---
+++ TaurusPOS/resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-2xl font-bold text-center">Bienvenido a Mi Aplicación estas en el home de vue </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
