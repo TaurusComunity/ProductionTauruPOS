d76a904d778c4d91710ead5f576637d7697d217c
JhoannS <sebastianzamudio2004@gmail.com>
2025-03-14 10:34:54 -0500

    Creacion de la tabla roles

---
+++ TaurusPOS/database/migrations/2025_03_14_142915_create_roles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id(); // ID autoincremental (equivalente a INT PRIMARY KEY AUTO_INCREMENT)
+            $table->unsignedBigInteger('id_estado')->default(1);
+            $table->enum('tipo_rol', ["Administrador", "Empleado", "Cliente"])->nullable(false);
+           
+           
+            $table->timestamp('fecha_creacion')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('fecha_modificacion')->default(DB::raw('CURRENT_TIMESTAMP'))->useCurrentOnUpdate();
+            
+            // Si necesitas una relación con otra tabla, por ejemplo, estados:
+            $table->foreign('id_estado')->references('id')->on('estados')->onDelete('cascade');
+            
+        });
+
+        DB::table('roles')->insert([
+            ['tipo_rol' => 'Administrador'],
+            ['tipo_rol' => 'Empleado'],
+            ['tipo_rol' => 'Cliente'],
+        ]);
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
