c921e0eef05589e310fcc1ebbd5b196a93dae459
JhoannS <sebastianzamudio2004@gmail.com>
2025-03-14 13:49:06 -0500

    Creacion de rutas junto a sus vistas de cliente, empleado y superAdmin

---
+++ TaurusPOS/resources/js/Pages/Cliente/Dashboard.vue
@@ -0,0 +1,19 @@
+<script>
+import { Head } from '@inertiajs/vue3';
+
+export default {
+    name: 'Dashboard',
+    components: {
+        Head,
+    },
+}
+</script>
+
+
+
+<template>
+  <title>Panel cliente</title>
+  <div class="grid place-items-center h-screen">
+    <h2>Panel Cliente</h2>
+  </div>
+</template>
---
+++ TaurusPOS/resources/js/Pages/Empleado/Dashboard.vue
@@ -0,0 +1,19 @@
+<script>
+import { Head } from '@inertiajs/vue3';
+
+export default {
+    name: 'Dashboard',
+    components: {
+        Head,
+    },
+}
+</script>
+
+
+
+<template>
+  <title>Panel empleado</title>
+  <div class="grid place-items-center h-screen">
+    <h2>Panel Empleado</h2>
+  </div>
+</template>
