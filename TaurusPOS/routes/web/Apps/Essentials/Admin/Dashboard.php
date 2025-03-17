a3ed2da6326a835833f9a3df0016da170f2360f0
JhoannS <sebastianzamudio2004@gmail.com>
2025-03-16 20:07:43 -0500

    Organizacion de carpetas para un mejor orden en cada componente

--- TaurusPOS/routes/web/Apps/Essentials/Admin/Dashboard.php
+++ TaurusPOS/routes/web/Apps/Essentials/Admin/Dashboard.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('essentials/admin')->group(function () {
     Route::get('/dashboard', function () {
-        return Inertia::render('Apps/Essentials/Admin/Dashboard');
+        return Inertia::render('Apps/Essentials/Admin/Dashboard/Dashboard');
     })->name('essentials.admin.dashboard');
 });
 
--- TaurusPOS/routes/web/Apps/Essentials/Admin/Multisucursales.php
+++ TaurusPOS/routes/web/Apps/Essentials/Admin/Multisucursales.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('essentials/admin')->group(function () {
     Route::get('/multisucursales', function () {
-        return Inertia::render('Apps/Essentials/Admin/Multisucursales');
+        return Inertia::render('Apps/Essentials/Admin/Multisucursales/Multisucursales');
     })->name('essentials.admin.multisucursales');
 });
